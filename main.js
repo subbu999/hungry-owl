51652db6c3b4791047019d57eeeda9189205e15a
subbu999 <b.subbu504@gmail.com>
2021-01-26 23:38:12 +0530

    contact form finished

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 $(document).ready(function () {
-
     AOS.init();
     $(".navbar-nav .nav-link").on("click", function () {
         $("#owlAudio").get(0).play();
         $(".navbar-nav").find(".activate").removeClass("activate");
         $(this).addClass("activate");
     });
-    const parallax = document.querySelector('.slide-area');
+
+
+    $('#contact-form').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLScZn0dLhXi-rgARJlTS8XOqEhm1r68NOvPYA4AKjbAd2wh6mw/formResponse",
+            data: $(this).serialize(),
+            type: "POST",
+            dataType: "xml",
+            success: function (data) {
+                console.log('Submission successful');
+            },
+            error: function (xhr, status, error) {
+                alert("Thank you for your feedback");
+                // $('#contact-form').trigger("reset");
+            }
+        });
+
+    });
 
 })
+
