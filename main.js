3b50cdaf33a277bb43ff4112efc37de8fd556a05
subbu999 <b.subbu504@gmail.com>
2021-01-26 14:04:37 +0530

    About section has completed

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
-    AOS.init()
+
+    AOS.init();
+    $(".navbar-nav .nav-link").on("click", function () {
+        $("#owlAudio").get(0).play();
+        $(".navbar-nav").find(".activate").removeClass("activate");
+        $(this).addClass("activate");
+    });
+    const parallax = document.querySelector('.slide-area');
+
 })
